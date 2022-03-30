24d2910e1d60b4cb2ec85312159d0029765ea534
sal-maa <salmaa.matematika@gmail.com>
2022-03-30 15:18:58 +0700

    add responsive and tidy up

--- js/script.js
+++ js/script.js
@@ -42,10 +42,10 @@ class Calculator {
             case '-':
                 computation = prev - current
                 break
-            case '*':
+            case '×':
                 computation = prev * current
                 break
-            case '/':
+            case '÷':
                 computation = prev / current
                 break
             default:
